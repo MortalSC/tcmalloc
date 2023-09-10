f2e8d1a8a864e780ad62b7b9cc9ff7b66834a801
MortalSC <mortal_scode@163.com>
2023-09-10 19:46:42 +0800

    初步完成PageCache部分的内存申请逻辑

--- tcmalloc/PageCache.cpp
+++ tcmalloc/PageCache.cpp
@@ -1,128 +1,67 @@
 #include "PageCache.h"
 
-PageCache PageCache::_sInstan;
+PageCache PageCache::_Instan;
 
+/*
+*	作用：从可用页中获取内存
+*/
+Span* PageCache::GetNewPage(size_t npage)
+{
+	assert(npage > 0 && npage < PAGENUMS);
 
-Span* PageCache::NewSpan(size_t num) {
-	/* 页数的合法判断 */
-	assert(num > 0 && num < NPAGES);
-
-	// 检查特定页是否有span
-	if (!_spanLists[num].Empty()) {
-		// 有 span 则分配！
-		return _spanLists->PopFront();
+	// 1. 判断指定的页中是否有可用内存！
+	// 若没有，则顺序查看下一页直到遇到非空页 / 最后一页（无可用内存，向堆申请）
+	if (!_spanListPage[npage].Empty()) {
+		return _spanListPage->PopFront();
 	}
 
-	// 走到此处，说明特定的页是空
-	// 此时，向后搜索非空页，进行切分并调整页
-	for (size_t i = num + 1; i < NPAGES; ++i) {
-		if (!_spanLists[i].Empty()) {
-			// 顺序找到第一个非空的桶！ => 开始分割，并调整页
-			// 分割：切分成 num 页的 span 和 一个 
-			Span* nSpan = _spanLists[i].PopFront();				// 拿到非空页中的第一个 span（具有特定大小）
+	// 2. 顺序查看下一页直到遇到非空页 / 最后一页（无可用内存，向堆申请）
+	for (size_t i = npage + 1; i < PAGENUMS; i++) {
+		if (!_spanListPage[i].Empty()) {
+			// 遇到非空页！
+			
+			// 页上的内存操作规则：该页分成两部分：
+			// 第一部分：申请对象时需求的页大小 + 剩余大小（向前挂在到PageCache中的特定页上）
+			Span* prevSpan = _spanListPage[i].PopFront();		// 获取到页中内存块
 			Span* kSpan = new Span;
 
-			// num 页分配给 central cache
-			kSpan->_page_id = nSpan->_page_id;					
-			kSpan->_n = num;
-			nSpan->_page_id += num;
-			nSpan->_n -= num;
+			// 在prevSpan的头部切一个 npage 页下来
+			// npage 页 npage 返回
+			// prevSpan再挂到对应映射的位置
 
-			_spanLists[nSpan->_n].PushFront(nSpan);				// 切后后的属于部分，调整到页管理的映射位置
+			// 获取到页 ID
+			kSpan->_pageID = prevSpan->_pageID;					// 本属于同一页中！分割后的新 span 需要记录页id
+			kSpan->_n = npage;									// 记录页号
 
-			// 存储未被分配出去的页单元的起始与终止页，便于page cache 回收内存
-			_idSpanMap[nSpan->_page_id] = nSpan;
-			_idSpanMap[nSpan->_page_id + nSpan->_n - 1] = nSpan;
+			prevSpan->_pageID += npage;							// 相对页id是向上递增！即：第128页的页id是最小值！
+			prevSpan->_n -= npage;
 
-			// 建立页 id 与 span 的映射，便于 central cache 回收小内存时，查找对应的span
-			for (PAGE_ID i = 0; i < kSpan->_n; i++) {
-				_idSpanMap[kSpan->_page_id + i] = kSpan;
+			_spanListPage[prevSpan->_n].PushFront(prevSpan);	// 大页（第一部分：对一开始的空页进行补充【看图更容易理解】）
+
+			// 存储 prevSpan 的首位页号跟 prevSpan 映射，方便 page cache 回收内存时
+			// 进行的合并查找：即记录了大块页的分配去向（便于回收合并）
+			_idSpanMap[prevSpan->_pageID] = prevSpan;
+			_idSpanMap[prevSpan->_pageID + prevSpan->_n - 1] = prevSpan;
+
+
+			// 建立id和span的映射，方便central cache回收小块内存时，查找对应的span
+			for (PAGE_ID i = 0; i < kSpan->_n; ++i)
+			{
+				_idSpanMap[kSpan->_pageID + i] = kSpan;
 			}
 
 			return kSpan;
 		}
 	}
 
-	// 走到此处，说明 page cache 中没有更大的可用页 span 了
-	// 此时，向堆申请 128 页 的span
+	// 3. 到此位置，即：直到最后一页页没有可用空间，需要向相同申请新的大页：去找堆要一个128页的span
 	Span* bigSpan = new Span;
+	void* ptr = SystemAlloc(PAGENUMS - 1);
+	bigSpan->_pageID = (PAGE_ID)ptr >> PAGE_SHIFT;		// 计算 Span 的页 ID ！
+	// 计算方法：通过地址（值） / 页大小基本单位（8kb）获取到相对页号！
+	bigSpan->_n = PAGENUMS - 1;							// 新获取的页号一定在第 128 页
 
-	void* ptr = SystemAlloc(NPAGES - 1);
-	bigSpan->_page_id = (PAGE_ID)ptr >> PAGE_SHIFT;
-	bigSpan->_n = NPAGES - 1;
+	_spanListPage[bigSpan->_n].PushFront(bigSpan);		// 把新页放入映射“阶梯”桶中
 
-	_spanLists[bigSpan->_n].PushFront(bigSpan);
-	return NewSpan(num);
-}
-
-Span* PageCache::MapObjectToSpan(void* obj) {
-	// 计算页号
-	PAGE_ID id = (PAGE_ID)obj >> PAGE_SHIFT;	
-	// 通过页号匹配span
-	auto ret = _idSpanMap.find(id);
-	if (ret != _idSpanMap.end()) {
-		// 找到映射
-		return ret->second;
-	}
-	else {
-		// 没找到！就是有问题（理论上不会出现）
-		assert(false);
-		return nullptr;
-	}
-}
-
-void PageCache::ReleaseSpanToPageCache(Span* span) {
-	// 对 span 前后的页，尝试进行合并，缓解内存碎片问题
-	while (1) {
-		PAGE_ID prev_id = span->_page_id = 1;					// 先向前探查
-		auto ret = _idSpanMap.find(prev_id);
-		if (ret == _idSpanMap.end()) {
-			// 前面的页号没有，不合并
-			break;
-		}
-		Span* prevSpan = ret->second;
-		if (prevSpan->isUse == true) {
-			// 存在响铃页，但是正在被使用
-			break;
-		}
-		// 若合并的大小大于 128 就不合并！
-		if (prevSpan->_n + span->_n >= NPAGES - 1) {
-			break;
-		}
-
-		span->_page_id = prevSpan->_page_id;
-		span->_n += prevSpan->_n;
-
-		_spanLists[prevSpan->_n].Erase(prevSpan);
-		delete prevSpan;
-	}
-
-	// 向后合并
-	while (1) {
-		PAGE_ID next_id = span->_page_id + span->_n - 1;
-		auto ret = _idSpanMap.find(next_id);
-		if (ret == _idSpanMap.end()) {
-			// 前面的页号没有，不合并
-			break;
-		}
-		Span* nextSpan = ret->second;
-		if (nextSpan->isUse == true) {
-			// 存在响铃页，但是正在被使用
-			break;
-		}
-		// 若合并的大小大于 128 就不合并！
-		if (nextSpan->_n + span->_n >= NPAGES - 1) {
-			break;
-		}
-
-		span->_n += nextSpan->_n;
-
-		_spanLists[nextSpan->_n].Erase(nextSpan);
-		delete nextSpan;
-	}
-
-	_spanLists[span->_n].PushFront(span);
-	span->isUse = false;
-	_idSpanMap[span->_page_id] = span;
-	_idSpanMap[span->_page_id + span->_n - 1] = span;
+	return GetNewPage(npage);
 }
